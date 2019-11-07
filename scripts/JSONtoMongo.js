f370bc54bfaede5784071c65e630d274c557fd85
Edwin_Carvajal <EdwinCarvajal@Edwins-MacBook-Pro-2.local>
2019-11-07 11:11:15 -0500

    Edited frontend index.html

---
+++ scripts/JSONtoMongo.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const fs = require('fs');
+const Product = require("../models/Product.js");
+const config = require('config');
+
+//connects our back end code with the database
+const db = config.get('mongoURI');
+mongoose.connect(db);
+
+fs.readFile('../datastore/ProductData.json', 'utf8', function (err, data) {
+    if (err) console.log(err);
+    var products = JSON.parse(data);
+    for (let i = 0; i < products.length; i++) {
+        var prod = new Product(products[i]);
+        prof.save(function (err) {
+            if (err) throw err;
+        });
+    }
+    console.log('finished');
+});
